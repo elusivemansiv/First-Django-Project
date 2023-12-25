338b537e89b027117f25f2c243a660890beab4c9
Khan,Mansib  Ahsan <97165527+elusivemansiv@users.noreply.github.com>
2023-12-25 22:03:54 +0600

    Add files via upload

---
+++ products/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Product
+from .models import Offer
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'discount')
+
+
+admin.site.register(Offer, OfferAdmin)
---
+++ products/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'products'
+
+
+
+class OffersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'offers'
